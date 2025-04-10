f533a782edc2e0d412b5e02703e2bfbbd580230a
ANGEL ANDRES HOYOS BOLIVAR <82899082+AngelHoyos@users.noreply.github.com>
2025-04-10 18:38:36 -0500

    Actualizar axiosInstance.ts

--- src/api/axiosInstance/axiosInstance.ts
+++ src/api/axiosInstance/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://alphatrader.onrender.com/',
+  baseURL: 'https://alphatraderback-esenfrgzcnbfbufw.brazilsouth-01.azurewebsites.net',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
